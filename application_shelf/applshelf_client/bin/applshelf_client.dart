7b08b1407d5bb3073c808cb009a3f68474efa30e
Philip Keller <philipugk@gmail.com>
2023-05-23 22:34:16 +0200

    small delay and look for specific port

--- application_shelf/applshelf_client/bin/applshelf_client.dart
+++ application_shelf/applshelf_client/bin/applshelf_client.dart
@@ -3,8 +3,10 @@ import 'package:applshelf_client/appshelf_serial.dart';
 
 void main() {
   final manager = AppManager();
-  getSerialConnection(portContains: 'usbserial')
-      .stream
-      .listen(getDataProcessCallback(manager));
-  print("makin connection");
+  Future.delayed(Duration(seconds: 3)).then((_) {
+    getSerialConnection(portContains: 'usbserial-1210')
+        .stream
+        .listen(getDataProcessCallback(manager));
+    print("makin connection");
+  });
 }
